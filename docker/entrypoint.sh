feec13ccbb7722ce01fae485432ac0a9d3a51886
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2024-10-02 23:26:26 +0500

    docker entrypoint updated

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,13 +3,9 @@ set -e
 # Переходим в директорию app
 cd app
 # Если есть файл .firstrun_php , то команда  composer --no-interaction install не выполняется
-if [ ! -f '.firstrun_php' ]; then
     export COMPOSER_ALLOW_SUPERUSER=1
-    touch '.firstrun_php'
     echo "Running composer install"
     composer --no-interaction install
-    php bin/console lexik:jwt:generate-keypair --skip-if-exists
-fi
 # команда, которая начинает слушать запросы на порту 8000
 php -S 0.0.0.0:$PORT $PUBLIC_DIR/$INDEX_FILE
 
